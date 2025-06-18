781e1e59ee68ce1f59f33ccd00a7fca1fcbe7106
Sreeharsh Rajan <sreeharshkrajan@gmail.com>
2025-06-18 20:23:51 +0530

    Added l5-swagger package for API documentation generation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,22 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
-{
-    //
-}
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Recipe Tracker API",
+ *     description="This is the API documentation for the Recipe Tracker project.",
+ *     @OA\Contact(
+ *         email="support@example.com"
+ *     )
+ * )
+ *  * @OA\SecurityScheme(
+ *     securityScheme="bearerAuth",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
+ * )
+ */
+abstract class Controller {}
